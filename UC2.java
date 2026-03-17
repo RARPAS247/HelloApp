04174bdaa3a98f0d8c401e9190981e22c5dd8260
Parth Shrikrishna Oak <parthoak12114@gmail.com>
2026-03-17 12:58:00 +0530

    Add files via upload

---
+++ UC2.java
@@ -0,0 +1,8 @@
+import java.rmi.server.SocketSecurityException;
+
+public class UC2 {
+    public static void main(String[] args) {
+        String name = "krs";
+        System.out.println("Hello "+name+"!");
+    }
+}
